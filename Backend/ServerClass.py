20a7f604aa66aaaf1397a2385278e8f28179e738
NPS <neelspd@gmail.com>
2021-05-30 10:51:26 +0200

    refactoring project, and adding ServerClass abstraction

--- Backend/ServerClass.py
+++ Backend/ServerClass.py
@@ -1,3 +1,17 @@
 import sqlite3 as database
+import asyncio
+import threading
+import time
 
 class ServerClass():
+    def connectToClient():
+        return 0
+
+    def sendMessages():
+        return 0
+
+    def receiveMessages():
+        return 0
+
+    def checkConnection():
+        return 0
