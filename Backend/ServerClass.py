41c23aaa8b0999fa13919d6f6052002f3fa6fddb
NPS <neelspd@gmail.com>
2021-05-30 13:58:45 +0200

    Commit to remane the frontend folder

--- Backend/ServerClass.py
+++ Backend/ServerClass.py
@@ -2,16 +2,29 @@ import sqlite3 as database
 import asyncio
 import threading
 import time
+import cryptography as crypt
 
-class ServerClass():
+class ServerConnectionClass():
     def connectToClient():
         return 0
+
+    def checkConnection():
+        return 0
+
+    def reconnectToClient():
+        return 0
+
+
+class InterruptionManagementClass():
+    def checkCalender():
+        return 0
+
+
+class ServerClass():
+
 
     def sendMessages():
         return 0
 
     def receiveMessages():
-        return 0
-
-    def checkConnection():
         return 0
